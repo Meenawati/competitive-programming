cf2755503e64a70a66d0cef794d33fc812bcdb74
Meenawati <meenawati.tudu@gmail.com>
2019-11-08 23:29:06 +0530

    given n find nth element of fibonacci series

---
+++ febonacci_sries_using_recursion.py
@@ -0,0 +1,12 @@
+def fabonacci(n):
+    if n < 0:
+        print("Invalid Input")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fabonacci(n-1)+fabonacci(n-2)
+
+
+print(fabonacci(10))
